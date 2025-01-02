ef99293ccc5938168a0d8c013248a8f1a56184ff
Paulo Henrique Calaes Oliveira <paulocalaes@gmail.com>
2025-01-02 16:51:19 +0000

    Add swagger config

--- project_parcellab/urls.py
+++ project_parcellab/urls.py
@@ -13,9 +13,43 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include, re_path
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Parcellab API",
+      default_version='v1',
+      description="API Documentation",
+      terms_of_service="https://www.parcellab.com/terms/",
+      contact=openapi.Contact(email="contact@parcellab.com"),
+      license=openapi.License(name="MIT License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+   authentication_classes=[TokenAuthentication]
+)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/<str:version>/', include([
+        path('shipments/', include('shipments.urls')),
+        path('weather/', include('weather.urls')),
+        path('articles/', include('articles.urls')),
+
+        path('auth/', include('djoser.urls')),  
+        path('auth/', include('djoser.urls.authtoken')),
+    ])),  
+
+    # Rotas do Swagger
+    re_path(r'^swagger/v1(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), {'version': 'v1'}, name='schema-json-v1'),
+    path('swagger/v1/', schema_view.with_ui('swagger', cache_timeout=0), {'version': 'v1'}, name='schema-swagger-ui-v1'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
